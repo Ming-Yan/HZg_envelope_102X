90f39655e8635015f7a0067845e12e4b8e1d13ad
Ming-Yan Lee <a342545@gmail.com>
2021-02-10 18:04:09 +0100

    add txt file

--- Fit_test/Maketree.h
+++ Fit_test/Maketree.h
@@ -5,23 +5,23 @@ TTree* makeTTree_signal(string fname)
 {
   // Create ROOT TTree filled with a Gaussian distribution in x and a uniform distribution in y
 
-  Double_t* mH = new Double_t;
+  Double_t* CMS_hzg_mass = new Double_t;
   Double_t* weights = new Double_t;
   TTree *T1 = new TTree("T1", "data from ascii file");
-  T1->Branch("mH", &mH, "mH/D"); // The branch name is m
+  T1->Branch("CMS_hzg_mass", &CMS_hzg_mass, "CMS_hzg_mass/D"); // The branch name is m
   T1->Branch("weights", &weights, "weights/D");
-  T1->ReadFile(fname.c_str(), "mH:weights");
+  T1->ReadFile(fname.c_str(), "CMS_hzg_mass:weights");
   return T1 ;
 }
 TTree* makeTTree(string fname)
 {
   // Create ROOT TTree filled with a Gaussian distribution in x and a uniform distribution in y
 
-  Double_t* mH = new Double_t;
+  Double_t* CMS_hzg_mass = new Double_t;
   Double_t* weights = new Double_t;
   TTree *T1 = new TTree("T1", "data from ascii file");
-  T1->Branch("mH", &mH, "mH/D"); // The branch name is m
-  T1->ReadFile(fname.c_str(), "mH");
+  T1->Branch("CMS_hzg_mass", &CMS_hzg_mass, "CMS_hzg_mass/D"); // The branch name is m
+  T1->ReadFile(fname.c_str(), "CMS_hzg_mass");
   return T1 ;
 }
 #endif
