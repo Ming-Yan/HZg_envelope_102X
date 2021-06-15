6836589279e5a547fcb875a33e31063d0b4526f4
Ming-Yan Lee <a342545@gmail.com>
2021-06-15 11:06:18 +0200

    update scripts general

--- Plots/sub_toys.sh
+++ Plots/sub_toys.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+read -p "cat: " cat
+read -p "postfit signal strength: " postr
 ulimit -s unlimited
 set -e
 cd /afs/cern.ch/user/m/milee/CMSSW_10_2_13/src/flashggFinalFit/Plots/SplusBModels1/toys
@@ -10,16 +12,16 @@ eval `scramv1 runtime -sh`
 for itoy in $(seq 0 99)
 do
 #Generate command
-combine /afs/cern.ch/user/m/milee/CMSSW_10_2_13/src/flashggFinalFit/Plots/cat1_post_snap.root -m 125.380 -M GenerateOnly --saveWorkspace --toysFrequentist --bypassFrequentistFit -t 1 --setParameters r=0.214 -s -1 -n _${itoy}_gen_step --snapshotName MultiDimFit
+combine /afs/cern.ch/user/m/milee/CMSSW_10_2_13/src/flashggFinalFit/Plots/cat${cat}_post_snap.root -m 125.380 -M GenerateOnly --saveWorkspace --toysFrequentist --bypassFrequentistFit -t 1 --setParameters r=${postr} -s -1 -n _${itoy}_gen_step --snapshotName MultiDimFit
 
 #Fit command
 mv higgsCombine_${itoy}_gen_step*.root gen_${itoy}.root
-root -l -b -q getsnap.C\(\"gen\"\,\"1\"\,${itoy}\)
-combine gen_${itoy}.root -m 125.380 -M MultiDimFit -P r --floatOtherPOIs=1 --saveWorkspace --toysFrequentist --bypassFrequentistFit -t 1 --setParameters r=0.214 -s -1 -n _${itoy}_fit_step --cminDefaultMinimizerStrategy 0 --X-rtd MINIMIZER_freezeDisassociatedParams --X-rtd MINIMIZER_multiMin_hideConstants --X-rtd MINIMIZER_multiMin_maskConstraints --X-rtd MINIMIZER_multiMin_maskChannels=2 --snapshotName "MultiDimFit"
+root -l -b -q getsnap.C\(\"gen\"\,\"${cat}\"\,${itoy}\)
+combine gen_${itoy}.root -m 125.380 -M MultiDimFit -P r --floatOtherPOIs=1 --saveWorkspace --toysFrequentist --bypassFrequentistFit -t 1 --setParameters r=${postr} -s -1 -n _${itoy}_fit_step --cminDefaultMinimizerStrategy 0 --X-rtd MINIMIZER_freezeDisassociatedParams --X-rtd MINIMIZER_multiMin_hideConstants --X-rtd MINIMIZER_multiMin_maskConstraints --X-rtd MINIMIZER_multiMin_maskChannels=2 --snapshotName "MultiDimFit"
 
 #Throw command
 mv higgsCombine_${itoy}_fit_step*.root fit_${itoy}.root
-root -l -b -q getsnap.C\(\"fit\"\,\"1\"\,${itoy}\)
+root -l -b -q getsnap.C\(\"fit\"\,\"${cat}\"\,${itoy}\)
 combine fit_${itoy}.root -m 125.380 --snapshotName MultiDimFit -M GenerateOnly --saveToys --toysFrequentist --bypassFrequentistFit -t -1 -n _${itoy}_throw_step --setParameters r=0
 
 mv higgsCombine_${itoy}_throw_step*.root toy_${itoy}.root
